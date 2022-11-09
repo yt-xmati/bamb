68f53e8c9d14c8d3130f12fc9ed5145b0812f86e
xmati <93664892+yt-xmati@users.noreply.github.com>
2022-11-09 22:47:47 +0100

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -172,9 +172,10 @@ class MainMenuState extends MusicBeatState
 
 	var selectedSomethin:Bool = false;
 
-
+}
 		var lerpVal:Float = CoolUtil.boundTo(elapsed * 7.5, 0, 1);
 		camFollowPos.setPosition(FlxMath.lerp(camFollowPos.x, camFollow.x, lerpVal), FlxMath.lerp(camFollowPos.y, camFollow.y, lerpVal));
+{
 
 		if (!selectedSomethin)
 		{
